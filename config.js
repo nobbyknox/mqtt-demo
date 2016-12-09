1544aaa9f67147c7615a19bf1a9e0b7850a093c1
Nobby Knox <nobbyknox@gmail.com>
2016-12-09 20:21:00 +0200

    Everything seems to have fallen into place

---
+++ config.js
@@ -0,0 +1,8 @@
+host = '139.59.133.244';    // hostname or IP address
+port = 9001;                // websocket port
+topic = 'vbx-con/2/#';      // topic to subscribe to
+useTLS = false;
+path = '/mqtt';
+username = null;
+password = null;
+cleansession = true;
