18db8f05fb3b08ea0ec62be5008e65b9ecdf179c
Nobby Knox <nobbyknox@gmail.com>
2016-12-16 19:42:18 +0200

    Change host IP for a more reliable demo

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-host = '139.59.133.244';    // hostname or IP address
+host = '172.0.0.13';        // hostname or IP address
 port = 9001;                // websocket port
 topic = 'vbx-con/2/#';      // topic to subscribe to
 useTLS = false;
